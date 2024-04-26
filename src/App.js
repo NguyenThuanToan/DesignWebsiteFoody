2584582217bb6228c0406dcc070ec7a26a38b182
NguyenThuanToan <Nguyenthuantoanb5@gmail.com>
2024-04-26 18:18:57 +0700

    Foody

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Work from './Components/Work';
+import About from './Components/About';
+import Home from './Components/Home';
+import Testimonials from './Components/Testimonials';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <About/>
+      <Work/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+const App = () =>{
+    return <MainContainer>
+
+    </MainContainer>
+}
+export default App;
